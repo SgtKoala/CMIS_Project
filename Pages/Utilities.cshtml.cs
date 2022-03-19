c6a0f9133273396f83d231b47fc2d78060da5070
Rendon Dasig <rendon.dasig98@gmail.com>
2022-03-19 08:22:46 +0800

    Add files via upload
    first commit

---
+++ Pages/Utilities.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CMISID_PROJECT.Pages
+{
+    public class UtilitiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
